7481646f4cd5c453b05474a4ddbbaecde47374db
macin-dev <ernesto.dev24@gmail.com>
2026-07-24 20:41:10 -0600

    feat(day-18): complete challenge 3 - draw shapes 3-10 sides

--- day-18-turtle-module/main.py
+++ day-18-turtle-module/main.py
@@ -1,16 +1,21 @@
+from random import choice
 from turtle import Turtle, Screen
 
 timmy = Turtle()
 my_screen = Screen()
 
 timmy.shape("turtle")
-timmy.color("green")
 
-# Draw a Dashed line
-for _ in range(10):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+# Draw a triangle, square, pentagon, hexagon,
+# heptagon, octagon, nonagon, and decagon
+colors = ["red", "blue", "green", "yellow", "orange", "pink", "purple"]
+
+# Draw shapes from 3 sides (triangle) up to 10 sides (decagon)
+for num_sides in range(3,11):
+    angle = 360 / num_sides
+    timmy.color(choice(colors))
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
 
 my_screen.exitonclick()
