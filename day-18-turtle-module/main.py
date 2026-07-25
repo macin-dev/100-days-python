b21853f25d888749fa2c5123872f5f35481cbf47
macin-dev <ernesto.dev24@gmail.com>
2026-07-24 21:23:15 -0600

    feat(day-18): add RGB random_color function to random walk

--- day-18-turtle-module/main.py
+++ day-18-turtle-module/main.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, randint
 from turtle import Turtle, Screen
 
 timmy = Turtle()
@@ -7,14 +7,21 @@ my_screen = Screen()
 timmy.shape("turtle")
 timmy.pensize(10)
 timmy.speed("fastest")
+my_screen.colormode(255)
+
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return (r, g, b)
+
 
 # Draw a Random Walk
-colors = ["red", "orange", "yellow", "green", "blue", "violet", "purple"]
 directions = [0, 90, 180, 270]
 
 for _ in range(100):
     timmy.right(choice(directions))
-    timmy.color(choice(colors))
+    timmy.color(random_color())
     timmy.forward(20)
 
 my_screen.exitonclick()
