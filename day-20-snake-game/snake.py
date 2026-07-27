ad89e90f3a674453a066ae2e113ee1f28e3dabb3
macin-dev <ernesto.dev24@gmail.com>
2026-07-27 14:40:42 -0600

    Control the snake and prevent 180-degree turns (Day 20 - Challenge 3)

--- day-20-snake-game/snake.py
+++ day-20-snake-game/snake.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
@@ -7,6 +11,7 @@ class Snake:
         self.segments = []
         self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         """Initialize the snake with three segments at the (0,0) position"""
@@ -23,4 +28,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
