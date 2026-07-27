574d05b148f5bf1cd16d26b8aecaf1e7ebcb9a09
macin-dev <ernesto.dev24@gmail.com>
2026-07-27 16:14:39 -0600

    Rename directory to day-20-21-snake-game and complete Snake Game challenge

--- day-20-21-snake-game/snake.py
+++ day-20-21-snake-game/snake.py
@@ -16,11 +16,17 @@ class Snake:
     def create_snake(self):
         """Initialize the snake with three segments at the (0,0) position"""
         for position in self.starting_positions:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def  extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         """Move the snake forward by 20 pixels"""
