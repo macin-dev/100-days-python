f2e929780e834ac086fd5ed9a98068604c90aa6e
macin-dev <ernesto.dev24@gmail.com>
2026-07-27 12:52:10 -0600

    Complete Day 19 Turtle Race challenge

--- day-19-turtle-module/main.py
+++ day-19-turtle-module/main.py
@@ -1,19 +1,40 @@
+import random
 from turtle import Turtle, Screen
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["pink", "red", "green", "blue", "purple", "orange"]
+all_turtles = []
 
 # Crete 6 instances of turtle
 y_axis = -70
 x_axis = -230
 
 for color in colors:
-    tim = Turtle(shape="turtle")
-    tim.color(color)
-    tim.penup()
-    tim.goto(x=x_axis, y=y_axis)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color)
+    new_turtle.penup()
+    new_turtle.goto(x=x_axis, y=y_axis)
     y_axis += 30
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+            break
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
 
 screen.exitonclick()
