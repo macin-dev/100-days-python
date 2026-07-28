ccb4a9ed2b30886b542ed5218f02a3e4e8c15635
macin-dev <ernesto.dev24@gmail.com>
2026-07-28 17:57:56 -0600

    Add paddle collision detection and horizontal bounce (Day 22 - Part 5)

--- day-22-pong-game/ball.py
+++ day-22-pong-game/ball.py
@@ -14,7 +14,9 @@ class Ball(Turtle):
         x = self.xcor() + self.x_move
         y = self.ycor() + self.y_move
         self.goto(x, y)
-        print(x, y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- day-22-pong-game/main.py
+++ day-22-pong-game/main.py
@@ -30,6 +30,10 @@ while game_is_on:
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
