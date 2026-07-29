1e88d6ec7a7c3a209c7c729d602802d69a94ba21
macin-dev <ernesto.dev24@gmail.com>
2026-07-29 12:21:20 -0600

    Add high score tracking and game reset capability (Day 24)

--- day-20-21-snake-game/scoreboard.py
+++ day-20-21-snake-game/scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.sety(270)
@@ -14,13 +15,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def display_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.display_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        self.high_score = max(self.score, self.high_score)
+        self.score = 0
+        self.display_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
